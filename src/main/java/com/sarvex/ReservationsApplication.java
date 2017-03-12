64da4181ae932c5ef87aa7eb1526986dc34a7560
Sarvex Jatasra <sarvex.jatasra@gmail.com>
2017-03-12 16:54:22 +0530

    Initial Version

---
+++ src/main/java/com/sarvex/ReservationsApplication.java
@@ -0,0 +1,12 @@
+package com.sarvex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ReservationsApplication.class, args);
+  }
+}
